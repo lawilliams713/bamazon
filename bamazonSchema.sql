2b7a621afbe4fc09d81f9b037778dbf821247b56
lawilliams713 <lawilliams713@gmail.com>
2019-06-24 21:45:40 -0500

    update sql

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -1,13 +1,15 @@
 DROP DATABASE IF EXISTS bamazon_db;
+
 CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
 
--- CREATE TABLE products(
---   id INT NOT NULL AUTO_INCREMENT,
---   product_name VARCHAR(100) NOT NULL,
---   category VARCHAR(45) NOT NULL,
---   starting_bid INT default 0,
---   highest_bid INT default 0,
---   PRIMARY KEY (id)
--- );
+CREATE TABLE products(
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  price DECIMAL(10,2) NULL,
+  quantity INT NULL,
+  PRIMARY KEY (id)
+);
+
